8c3b25a392400b99c35b02740d1cc966f751d8eb
matheus-oliveira-holu <matheus.oliveira@holu.com.br>
2024-08-02 12:00:14 -0300

    :sparkles: create tables if not exists

--- src/db.js
+++ src/db.js
@@ -8,4 +8,45 @@ const pool = new Pool({
   port: process.env.PORT,
 })
 
+async function createTables(){
+  try {
+    await pool.connect();
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS "manage-task".users (
+        id UUID PRIMARY KEY,
+        name VARCHAR(255) NOT NULL, 
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        dateBirth DATE NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS "manage-task".taskCategory (
+        id UUID PRIMARY KEY,
+        displayName VARCHAR(255) NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS "manage-task".taskStatus (
+        id UUID PRIMARY KEY,
+        displayName VARCHAR(255) NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS "manage-task".task (
+        id UUID PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        createDate DATE NOT NULL,
+        category_id UUID REFERENCES "manage-task".taskCategory(id),
+        status_id UUID REFERENCES "manage-task".taskStatus(id),
+        user_id UUID REFERENCES "manage-task".users(id)
+      );
+    `)
+    console.log("todas as tabelas criadas");
+  } catch (error) {
+    console.log(error);
+    throw error
+  }finally{
+    await pool.end()
+  }
+}
+
+createTables()
 module.exports = pool
