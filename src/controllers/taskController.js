07f10fcbd685db606fbfa6e3953a2627b88e6fcf
matheus-oliveira-holu <matheus.oliveira@holu.com.br>
2024-08-02 11:59:46 -0300

    :sparkles: Test the creation task before bank modeling

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,6 +1,29 @@
-const { getTasks } = require("../models/task");
+const { getTasks, createTask } = require("../models/task");
 
-const createTaskController = async (req, res) =>{}
+const createTaskController = async (req, res) =>{
+  try {
+    let body = '';
+    let title = ''
+    req.on('data', (chunk) => { //define um evento que é acionado sempre que um novo chunk de dados é recebido.
+      body += chunk;
+      console.log(body);
+    });
+    req.on('end', async () => { //O evento end é acionado quando todos os dados da requisição foram recebidos.
+      const parsedBody = JSON.parse(body);
+      title = parsedBody.title;
+      try {
+        await createTask(title)
+        res.writeHead(201, { 'Content-Type': 'text/plain' });
+        res.end('Tarefa criada com sucesso!');
+      } catch (error) {
+        res.statusCode = 500;
+        res.end(`Erro interno do servidor ${error}`);
+      }
+    }); 
+  } catch (error) {
+    
+  }
+}
 
 const getTasksController = async (req, res) =>{
   // logic to get tasks
--- src/models/task.js
+++ src/models/task.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
-const pool = require('../db')
+const pool = require('../db');
+const uuid4 = require('../utils/uuid');
+
+async function createTask(title){
+  if(!title){
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Titulo da tarefa não recebido');
+  }
+  try {
+    const id = uuid4()
+    console.log(id);
+    console.log(title);
+    const query = `INSERT INTO "manage-task".task("ID", name) VALUES('${id}','${title}')`
+    await pool.query(query)
+  } catch (error) {
+    console.log("Erro ao criar `tasks`: ", error); 
+    throw error
+  }
+}
 
 async function getTasks(){
   try {
-    const query = 'SELECT * FROM "manage-task".task'
+    const query = `SELECT * FROM "manage-task".task`
     const result = await pool.query(query)
     return result.rows;
   } catch (error) {
@@ -14,4 +32,5 @@ async function getTasks(){
 
 module.exports = {
   getTasks,
+  createTask
 }
